47828a6ee5f9f1b94fa8823e1e048c5b1264d38e
dansbands <dansbands@hotmail.com>
2019-04-26 12:00:49 -0400

    Updates

---
+++ precache-manifest.1845a47f24c9971fe01aa2c81489d701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46732b090ce1bfdee876",
+    "url": "/react-todo-app2/static/js/runtime~main.f3aa6e9c.js"
+  },
+  {
+    "revision": "8587bc26c81025ac534c",
+    "url": "/react-todo-app2/static/js/main.81e3450d.chunk.js"
+  },
+  {
+    "revision": "d463146f327e501ab794",
+    "url": "/react-todo-app2/static/js/2.1d327b0e.chunk.js"
+  },
+  {
+    "revision": "8587bc26c81025ac534c",
+    "url": "/react-todo-app2/static/css/main.af50f4be.chunk.css"
+  },
+  {
+    "revision": "9d78e2944ecaf09bc638316ef9dd7b0e",
+    "url": "/react-todo-app2/index.html"
+  }
+];
